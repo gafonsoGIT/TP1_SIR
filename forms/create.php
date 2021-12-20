8f83a9544828280eea0338afbecc43a0bcac11ce
gafonso23826 <79472102+gafonso23826@users.noreply.github.com>
2021-12-20 17:45:21 +0000

    crud

---
+++ forms/create.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho Prático</title>
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
+<body>
+    Hello world
+</body>
+    <div class="container">
+      <form id="formulario">
+        <label for="descricao">Descrição:</label>
+        <input type="text" id="descricao" />
+        <p>
+        <label for="nformacao">Informação:</label>
+        <input type="text" id="informacao" />
+        </p>
+        <p>
+        <label for="tipo">Tipo:</label>
+        <input type="text" id="tipo" />
+        </p>
+      </form>
+    </div>
+</html>
--- main.php
+++ main.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabalho Prático</title>
-    <link rel="stylesheet" href="./login/login.css">
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 <body>
 <?php
@@ -18,5 +18,9 @@
     <form method="POST" action="main.php">
         <a class="button" href="index.php">Terminar sessão</a> 
     </form>
+
+    <h1>Lista de Apontamentos</h1>
+      <a class="" href="./forms/create.php">Adicionar apontamento</a>
+    
 </body>
 </html>
